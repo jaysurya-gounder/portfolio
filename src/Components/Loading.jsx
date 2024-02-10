17725f8e5e15103898e9a473eeb4ac117d70d8dc
jaysurya-gounder <190770116031@socet.edu.in>
2024-02-10 21:04:50 +0530

    loading update

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loading = () => {
     return (
-        <div className='w-full h-screen bg-[rgb(27_27_27)] grid place-content-center'>
+        <div className='w-full h-screen bg-[rgb(27_27_27)] grid place-content-center max-md:h-screen'>
             <div className='text-3xl tracking-[30px] text-white max-md:text-2xl max-md:ps-5 max-md:tracking-[20px] loader'></div>
         </div>
     )
